bb74860b9b9bc6ff0d6fe733495686bbe4f0affa
matheus-ferreira <matheus.ferreira@jumplabel.com.br>
2020-02-28 16:10:39 -0300

    versionamento

--- wwwroot/js/NavItems/Tecnologias.js
+++ wwwroot/js/NavItems/Tecnologias.js
@@ -1,32 +1,45 @@
-﻿function ocultMods() {
-    addEffectFadeUp();
+﻿$('#h2-data-enginner-big-data').css({ 'color': '#ef8822' })
+
+function addEffectFadeUp() {
+    $('.img-col-6-mobile').removeClass('aos-init aos-animate');
+    $('.img-col-6-mobile').attr({ 'data-aos': 'fade-up' })
+    setTimeout(function () { $('.img-col-6-mobile').addClass('aos-init aos-animate'); }, 100)
+}
+
+function ocultMods() {
     $('#data-enginner-big-data,#data-science-machine-learning,#data-visualization,#robot-process-automation-solutions').hide();
 };
 
 $('#h2-data-enginner-big-data').on('click', function () {
     ocultMods();
     $('#data-enginner-big-data').show();
+    $(this).css({ 'color': '#ef8822' });
+    $('#h2-data-science-machine-learning,#h2-data-visualization,#h2-robot-process-automation-solutions').css({ 'color': '#000' });
 });
 
 $('#h2-data-science-machine-learning').on('click', function () {
     ocultMods();
     $('#data-science-machine-learning').show();
+    $(this).css({ 'color': '#ef8822' });
+    $('#h2-data-enginner-big-data,#h2-data-visualization,#h2-robot-process-automation-solutions').css({ 'color': '#000' });
+    addEffectFadeUp();
 });
 
 $('#h2-data-visualization').on('click', function () {
     ocultMods();
     $('#data-visualization').show();
+    $(this).css({ 'color': '#ef8822' });
+    $('#h2-data-enginner-big-data,#h2-data-science-machine-learning,#h2-robot-process-automation-solutions').css({ 'color': '#000' });
+    addEffectFadeUp();
 });
 
 $('#h2-robot-process-automation-solutions').on('click', function () {
     ocultMods();
-    $('#robot-process-automation-solutions').show();
+    $('#robot-process-automation-solutions').show()
+    $(this).css({ 'color': '#ef8822' });
+    $('#h2-data-enginner-big-data,#h2-data-science-machine-learning,#h2-data-visualization').css({ 'color': '#000' });
+    addEffectFadeUp();
 });
-
-function addEffectFadeUp() {
-    $('img').removeClass('aos-init aos-animate');
-    $('img').addClass('aos-init aos-animate');
-}
 
 function widthMenor700px() {
     if ($(window).width() < 768) {
